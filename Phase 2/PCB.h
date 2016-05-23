1c54a516b1e7d5278c8eb01353b6f87beb2c22ee
Aaron Chamberlain <aaronjchamberlain@gmail.com>
2016-05-22 22:30:07 -0700

    merging PCB.h

--- Phase 2/PCB.h
+++ Phase 2/PCB.h
@@ -1,3 +1,13 @@
+/* *********************** 
+PCB.h
+Written by Aaron Chamberlain & Adrian Osuna May 2016 at CSUSB
+The PCB is the status container of running processes within the OS.
+Important values are read/written to the PCB to keep track of state, including:
+Process Related: pc,r[0]-r[3],sr,sp,base,limit,process name,fstreams
+Accounting Info: CPU (VM), Waiting, Turnaround, I/O Time(s), & Max Stack Size
+All Accounting plus VM Utilization & Throughput must appear at end of .out file
+*************************/
+
 #ifndef PCB_H
 #define PCB_H 
 
@@ -6,6 +16,7 @@
 using namespace std;
 
 class PCB{
+public:    
 	//get registers from vm in order to store their state and contents
     static const int REG_FILE_SIZE = 4;
 	vector<int> reg;
@@ -24,6 +35,16 @@ class PCB{
     int stackSize;
 
 	friend class os;
+
+    PCB(); //default constructor
+    void copyPCBdata(); //function to move all data from runtime to PCB
+    void readPCBdata(); //function to move all data from 22
+    void checkPCBuse(); //if anything has been set the program has been in use.
+                        //if all zero then in VMrun() init to standard values
+    
+
+private:
+
 };
 
 #endif
